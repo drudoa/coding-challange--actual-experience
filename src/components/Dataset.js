30628d45e8a8357e07f2470a2c062e5de6449e29
drudoa <>
2020-05-13 14:10:18 +0100

    use new redux struct

--- src/components/Dataset.js
+++ src/components/Dataset.js
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { datasetFetchAsync } from "../redux/actions"
+import { fetchDataset } from "../redux/actions/dataset"
 
 export default () => {
   const dataset = useSelector((state) => state.dataset)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(datasetFetchAsync())
+    dispatch(fetchDataset())
   }, [dispatch])
 
-  if (isLoading) {
+  if (dataset.isFetching) {
     return <p>loading</p>
-  } else if (hasError) {
+  } else if (dataset.error) {
     return <p>error</p>
-  } else if (dataset.length > 0) {
+  } else if (dataset.data.length > 0) {
     return (
       <ul>
-        {dataset.map((data, index) => (
+        {dataset.data.map((data, index) => (
           <li key={index}>{data.category}</li>
         ))}
       </ul>
