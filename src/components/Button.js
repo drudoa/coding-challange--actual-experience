8ada6c3b8105f75e3dc85d78a82c72929cea71fc
drudoa <>
2020-05-14 18:02:06 +0100

    styled button

---
+++ src/components/Button.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+const Button = styled.button(({ styles, color, backgroundColor }) => ({
+  borderRadius: 4,
+  minWidth: 64,
+  boxSizing: "border-box",
+  padding: "6px 16px",
+  outline: 0,
+  border: 0,
+  backgroundColor: backgroundColor ?? "rgba(25,118,210)",
+  color: color ?? "#fff",
+  textTransform: "uppercase",
+  letterSpacing: "0.02857em",
+  fontSize: "0.875rem",
+  lineHeight: 1.75,
+  cursor: "pointer",
+  transition:
+    "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+  boxShadow:
+    "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14)",
+  "&:hover": {
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14)",
+    backgroundColor: "rgb(17, 82, 147)",
+  },
+  "&:active": {
+    boxShadow:
+      "0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14)",
+    backgroundColor: "rgb(25,118,210, 0.8)",
+  },
+  ...styles,
+}))
+
+export default Button
