44f3f4f663dab1d3868cb940015fbc6750785d77
drudoa <>
2020-05-16 22:48:32 +0100

    impl. barchart

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import PostcodeSearch from "../components/PostcodeSearch"
 import CrimeMap from "../components/CrimeMap"
+import CrimeChart from "../components/CrimeBarChart"
 import { useSelector, useDispatch } from "react-redux"
 import { fetchDataset } from "../redux/actions/dataset"
 
@@ -18,13 +19,23 @@ const Main = () => {
   }, [postcode, dispatch])
 
   return (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{
+        // display: "flex",
+        maxWidth: 960,
+        margin: "0 auto",
+        paddingTop: 28,
+      }}
+    >
+      <PostcodeSearch />
       <CrimeMap
         data={data}
         center={postcode ? [postcode.latitude, postcode.longitude] : undefined}
         zoom={postcode ? 14 : undefined}
       />
-      <PostcodeSearch />
+      <CrimeChart data={data} identity="category" />
+      {/* <div style={{ flex: 1 }}>
+      </div> */}
     </div>
   )
 }
